691e9894dff1a964534395e6f39ebfb038f750a5
Ernesto Pang <ernestopang513@hotmail.com>
2021-02-20 19:16:54 -0600

    Agregue el docs

---
+++ docs/precache-manifest.252b1d3461e7ad32c1f547b9c70bf9b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b04c5a22da48075cbc077e65a2a8770b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "341a0a77e1b8bb6cb2b7",
+    "url": "/static/css/main.1cf694d6.chunk.css"
+  },
+  {
+    "revision": "b380063bf79d894dd30d",
+    "url": "/static/js/2.e9167b08.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/static/js/2.e9167b08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "341a0a77e1b8bb6cb2b7",
+    "url": "/static/js/main.b63a1a2a.chunk.js"
+  },
+  {
+    "revision": "4e3a1f73b6e5de40b557",
+    "url": "/static/js/runtime-main.5c054f69.js"
+  }
+]);
